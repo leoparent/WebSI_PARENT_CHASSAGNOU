28baad08a570f797ed29067a9156fc8ae9991832
leoparent <leo.parent94@gmail.com>
2022-10-05 12:10:45 +0200

    ajout final lab2

--- handles.js
+++ handles.js
@@ -1,19 +1,37 @@
 // ./handles.js
 // Necessary imports
+const url = require('url')
+const qs = require('querystring')
+const {readFileSync}=require('fs')
+
+
 module.exports = {
-    serverHandle: function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
+  serverHandle: function (req, res) {
+    const route = url.parse(req.url)
+    const path = route.pathname 
+    const params = qs.parse(route.query)
+    const user = JSON.parse(readFileSync("./content/about.json",'utf-8'));
+    res.writeHead(200, {'Content-Type': 'text/plain'});
   
-  res.end();
-    } 
+    if (path === '/hello' && 'name' in params) {
+      res.write('Hello ' + params['name'])
+      if(params['name']=== 'leo')
+      {
+        res.write('\n')
+        res.write('vous etes sur le lab de leo et pa')
+      }
+
+    } else {
+      
+    }
+    res.write('\n') 
+    res.write(user.title)
+    res.write('\n') 
+    res.write(user.content)
+    res.write('\n')
+    res.write(user.author)
+    res.write('\n')
+    res.write(user.date)
+    res.end();
   }
+}
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// Import a module
+// ./index.js
 const http = require('http')
 const handles = require('./handles')
-const url = require('url')
-const qs = require('querystring')
-
 const server = http.createServer(handles.serverHandle);
-  server.listen(8080)
+server.listen(8080)
