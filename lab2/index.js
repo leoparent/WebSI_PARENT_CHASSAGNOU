b1a4fa73faec52f34bdff278fefde29bf7090800
Paul-Adrien Cha <78745732+HeadjuiceOne@users.noreply.github.com>
2022-10-05 11:40:35 +0200

    Delete lab2 directory

--- lab2/index.js
+++
@@ -1,8 +0,0 @@
-// Import a module
-const http = require('http')
-const handles = require('./handles')
-const url = require('url')
-const qs = require('querystring')
-
-const server = http.createServer(handles.serverHandle);
-  server.listen(8080)
