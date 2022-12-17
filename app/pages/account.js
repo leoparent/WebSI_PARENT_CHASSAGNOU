b78874bcb6394bd68c651b9c7b3aa3ab426b3167
headjuiceone <pauladrien.chassagniou@edu.ece.fr>
2022-12-17 16:59:42 +0100

    feat : connecting bug fixed

--- app/pages/account.js
+++ app/pages/account.js
@@ -1,50 +1,59 @@
-import { useState, useEffect, useContext } from 'react'
-import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
-import UserAvatar from '../components/Gravatar'
-import Link from 'next/link'
+import { useState, useEffect, useContext } from "react";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
+import UserAvatar from "../components/Gravatar";
 
 export default function Account({ session }) {
-  const supabase = useSupabaseClient()
-  const user = useUser()
-  const [loading, setLoading] = useState(true)
-  const [username, setUsername] = useState(null)
-  const [website, setWebsite] = useState(null)
-  const [avatar_url, setAvatarUrl] = useState(null)
+  const supabase = useSupabaseClient();
+  const user = useUser();
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState(null);
+  const [website, setWebsite] = useState(null);
+  const [avatar_url, setAvatarUrl] = useState(null);
 
   useEffect(() => {
-     user ? getProfile() : updateProfile({username,website,avatar_url})  
-  }, [session])
+    getProfile()
+  }, [session]);
 
   async function getProfile() {
     try {
-      setLoading(true)
+      setLoading(true);
+
+      const inserts = {
+        id: user.id,
+        username,
+        website,
+        avatar_url,
+        updated_at: new Date().toISOString(),
+      };
+
+      let { err } = await supabase.from("profiles").insert(inserts);
 
       let { data, error, status } = await supabase
-        .from('profiles')
+        .from("profiles")
         .select(`username, website, avatar_url`)
-        .eq('id', user.id)
-        .single()
+        .eq("id", user.id)
+        .single();
 
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
       if (data) {
-        setUsername(data.username)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
       }
     } catch (error) {
-      alert('Error loading user data!')
-      console.log(error)
+      alert("Error loading user data!");
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
   async function updateProfile({ username, website, avatar_url }) {
     try {
-      setLoading(true)
+      setLoading(true);
 
       const updates = {
         id: user.id,
@@ -52,24 +61,24 @@ export default function Account({ session }) {
         website,
         avatar_url,
         updated_at: new Date().toISOString(),
-      }
+      };
 
-      let { error } = await supabase.from('profiles').upsert(updates)
-      if (error) throw error
+      let { error } = await supabase.from("profiles").upsert(updates);
+      
     } catch (error) {
-      alert('Error updating the data!')
-      console.log(error)
+        alert("Error updating the data!");
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
   return (
-    <div className = "bg-gray-300 rounded-3xl py-10 flex justify-center items-center flex-col">
+    <div className="bg-gray-300 rounded-3xl py-10 flex justify-center items-center flex-col">
       <h1 className="font-medium text-gray-600 pb-10"> Account Settings</h1>
-            
-      <UserAvatar email={session.user.email} size={200} /> 
-            
+
+      <UserAvatar email={session.user.email} size={200} />
+
       <div className="py-5">
         <label htmlFor="email">Email : </label>
         <input id="email" type="text" value={session.user.email} disabled />
@@ -79,35 +88,38 @@ export default function Account({ session }) {
         <input
           id="username"
           type="text"
-          value={username || ''}
+          value={username || ""}
           onChange={(e) => setUsername(e.target.value)}
         />
       </div>
-      <div className='py-5'>
+      <div className="py-5">
         <label htmlFor="website">Website : </label>
         <input
           id="website"
           type="website"
-          value={website || ''}
+          value={website || ""}
           onChange={(e) => setWebsite(e.target.value)}
         />
       </div>
 
-      <div className='py-5'>
+      <div className="py-5">
         <button
           className="rounded-lg p-2  bg-green-500 hover:bg-green-700"
           onClick={() => updateProfile({ username, website, avatar_url })}
           disabled={loading}
         >
-          {loading ? 'Loading ...' : 'Update'}
+          {loading ? "Loading ..." : "Update"}
         </button>
       </div>
 
       <div className="py-5">
-        <button className="rounded-lg p-2 bg-red-600 hover:bg-red-800" onClick={() => supabase.auth.signOut()}>
+        <button
+          className="rounded-lg p-2 bg-red-600 hover:bg-red-800"
+          onClick={() => supabase.auth.signOut()}
+        >
           Sign Out
         </button>
       </div>
     </div>
-  )
+  );
 }
