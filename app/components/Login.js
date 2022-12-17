3410177a7fecf6c9ba882058bc5db09054828668
headjuiceone <pauladrien.chassagniou@edu.ece.fr>
2022-12-17 17:20:24 +0100

    feat : update Header with New+ button

--- app/components/Login.js
+++ app/components/Login.js
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import UserAvatar from './Gravatar'
 import { useUser, useSupabaseClient, useSession } from '@supabase/auth-helpers-react'
 import { useEffect } from 'react'
+import Link from 'next/link'
 
 export default function LoggedIn({session}){
   const router = useRouter()
@@ -17,24 +18,30 @@ export default function LoggedIn({session}){
       router.push('/login')
   }
   return (
-    <button
-      className="flex gap-2 [&_svg]:h-6 [&_svg]:w-6"
-      onClick={onClick}
-    >
-      { session ?
-        <>
-          <span
-            className="pl-10 hover:to-black hover:opacity-70">
-            <UserAvatar email={session.user.email} size={50} />
-          </span>
-        </>
-        :
-        <>
-          <span
-            className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
-          Connect </span>
-        </>
-      }
-    </button>
+    <>
+      <div className='flex gap-2 [&_svg]:h-6 [&_svg]:w-6 items-center'>
+        { session ?
+          <>
+            <button  className="ml-8 rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
+              <Link href="./article">New + </Link>
+            </button>
+            <button className="flex gap-2 [&_svg]:h-6 [&_svg]:w-6 items-center"onClick={onClick}>
+              <span
+                className="pl-10 hover:to-black hover:opacity-70">
+              <UserAvatar email={session.user.email} size={50} />
+              </span>
+            </button>
+          </>
+          :
+          <>
+            <button className="flex gap-2 [&_svg]:h-6 [&_svg]:w-6 items-center"onClick={onClick}>
+              <span className="ml-8 rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
+                Connect 
+              </span>
+            </button>
+          </>
+        }
+      </div>
+    </>
   )
 }
