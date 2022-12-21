e3376d5f0d0ca2c71c860d67036b0c9d7db45321
leoparent <leo.parent94@gmail.com>
2022-12-21 14:08:10 +0100

    feat: accent color section

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        'theme-blanc': '#FFFFFF',
+        'theme-violet': '#3B2077',
+        'theme-jaune':'#F1BA29',
+        'theme-bleu': '#235A8C',
+        'theme-vert': '#49B675',
+      },
       animation: {
         wiggle: 'wiggle 1s ease-in-out infinite',
       }
