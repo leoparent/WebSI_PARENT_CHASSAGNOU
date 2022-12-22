e3dfcca170b431256ccf44bdbbd2f0dd80bb4891
headjuiceone <pauladrien.chassagniou@edu.ece.fr>
2022-12-22 16:08:09 +0100

    feat : deployment fix

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -4,10 +4,12 @@ import 'tailwindcss/tailwind.css'
 import UserContext from '../contexts/UserContext'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { createClient } from "@supabase/supabase-js"
 
 function MyApp({ Component, pageProps }) {
 
-  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL , process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+  //const [supabase] = useState(() => createBrowserSupabaseClient())
   const [user, setUser] = useState("")
   const [isLogged, setLog] = useState(false)
   const [darkMode, setDarkMode] = useState(false)
